92728df65208229a70e70c2bbaafd16eed0cfe1e
Mikhail Lapshin <lapshin.michail@gmail.com>
2016-01-11 16:33:13 +0300

    пофиксил запрос получения суммы со счета

--- app/Запросы/Счета/Сумма на счету.sql
+++ app/Запросы/Счета/Сумма на счету.sql
@@ -2,9 +2,10 @@
  * @author minya92
  * @name qGetAccountBalance
  */ 
-
-Select t1.account_id, sum(t1.operation_sum * t.multiplier) as account_balance
-From bill_operations t1
- Inner Join bill_operations_type t on t1.operation_type = t.bill_operations_type_id
-where (t1.operation_status = 1) and :account_id = t1.account_id
-group by t1.account_id
+Select t1.bill_accounts_id as account_id, case when sum(t2.operation_sum * t.multiplier) is null then 0 else sum(t2.operation_sum * t.multiplier) end AS account_balance 
+From bill_accounts t1
+ Left Join bill_operations t2 on t1.bill_accounts_id = t2.account_id
+ Left Join bill_operations_type t on t2.operation_type = t.bill_operations_type_id
+ Where (t2.operation_status = 1 or t2.operation_status is null)
+ and  :account_id = t1.bill_accounts_id
+ Group by t1.bill_accounts_id
