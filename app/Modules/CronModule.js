14661f853cd37c0612439c12f7f7e1dfa99c271a
Mikhail Lapshin <lapshin.michail@gmail.com>
2016-11-09 14:44:34 +0300

    tksrv-105 не рабоет cron как задумано

--- app/Modules/CronModule.js
+++ app/Modules/CronModule.js
@@ -20,86 +20,92 @@ define('CronModule', ['orm', 'rpc', 'Messages', 'Events', 'logger'],
                  * Проверить заплаированные операции на сегодня
                  * date - 11.23.2016
                  */
-                self.CheckPlannedPayments = function (aDate, cb) {
+                self.CheckPlannedPayments = function (aDate, callback) {
+                    var res = [], count = 0;
+                    function cb(param, i){
+                        res.push(param);
+                        count = count + 1;
+                        if(count > model.qListPlannedOperations.length || i == -1){
+                            model.save();
+                            callback({response: res});
+                        }
+                    }
                     model.qListPlannedOperations.params.pDate = aDate ? new Date(aDate) : null;
                     model.qListPlannedOperations.requery();
                     if (model.qListPlannedOperations.length) {
                             for (var i = 0; i < model.qListPlannedOperations.length; i++) {
                                 (function (i) {
-                                    accounts.getSumFromAccount(model.qListPlannedOperations[i].account_id, function (account) {
-                                        if (!account.error) {
-                                            var serviceAccountId = model.qListPlannedOperations[i].bill_services_accounts;
-                                            model.qAddService.params.service_account_id = serviceAccountId;
-                                            model.qAddService.requery();
-                                            if(model.qAddService.length){
-                                                var serviceCost = model.qAddService.cursor.service_cost;
-                                                if (account.sum >= serviceCost) {
-                                                    if(model.qListPlannedOperations[i].bill_services_accounts){
-                                                        if(model.qAddService.cursor.service_days || model.qAddService.cursor.service_month){
-                                                            if(!model.qAddService.cursor.paused){
-                                                                var date;
-                                                                if(aDate)
-                                                                    date = new Date(aDate);
-                                                                else
-                                                                    date = new Date();
-                                                                Log.info('\n !!!!!!!!!' + date);
-                                                                if (model.qAddService.cursor.service_month)
-                                                                    date.setMonth(date.getMonth() + 1);
-                                                                else
-                                                                    date.setDate(date.getDate() + model.qAddService.cursor.service_days);
-                                                                Log.info('\n !!!!!!!!!!' + date);
-                                                                var accountId = model.qAddService.cursor.account_id;
-                                                                var serviceId = model.qAddService.cursor.service_id;
-                                                                operations.createOperation(accountId, serviceCost, true, serviceAccountId, null, function(op){
-                                                                    if(op.id){
-                                                                        operations.setOperationPlanned(op.id, date, function(){
-                                                                            cb({res: 'planned done'});
-                                                                        }, function(){
-                                                                            cb({error: 'planned err'});
-                                                                        });
-                                                                    }
-                                                                }, function(){
-                                                                    services.PauseOrResumeService(null, null, serviceAccountId, true, function () {
-                                                                        cb({res: 'pause done'});
+                                    model.qGetAccountBalance.params.account_id = model.qListPlannedOperations[i].account_id;
+                                    model.qGetAccountBalance.requery();
+                                    if (model.qGetAccountBalance.length){
+                                        var serviceAccountId = model.qListPlannedOperations[i].bill_services_accounts;
+                                        model.qAddService.params.service_account_id = serviceAccountId;
+                                        model.qAddService.requery();
+                                        if(model.qAddService.length){
+                                            var serviceCost = model.qAddService.cursor.service_cost;
+                                            if (model.qGetAccountBalance[0].account_balance >= serviceCost) {
+                                                if(model.qListPlannedOperations[i].bill_services_accounts){
+                                                    if(model.qAddService.cursor.service_days || model.qAddService.cursor.service_month){
+                                                        if(!model.qAddService.cursor.paused){
+                                                            var date;
+                                                            if(aDate)
+                                                                date = new Date(aDate);
+                                                            else
+                                                                date = new Date();
+                                                            Log.info('\n !!!!!!!!!' + date);
+                                                            if (model.qAddService.cursor.service_month)
+                                                                date.setMonth(date.getMonth() + 1);
+                                                            else
+                                                                date.setDate(date.getDate() + model.qAddService.cursor.service_days);
+                                                            Log.info('\n !!!!!!!!!!' + date);
+                                                            var accountId = model.qAddService.cursor.account_id;
+                                                            operations.createOperation(accountId, serviceCost, true, serviceAccountId, null, function(op){
+                                                                if(op.id){
+                                                                    operations.setOperationPlanned(op.id, date, function(){
+                                                                        cb({res: 'planned done'}, i);
                                                                     }, function(){
-                                                                        cb({error: 'pause err'});
+                                                                        cb({error: 'planned err'}, i);
                                                                     });
+                                                                }
+                                                            }, function(){
+                                                                services.PauseOrResumeService(null, null, serviceAccountId, true, function () {
+                                                                    cb({res: 'pause done'}, i);
+                                                                }, function(){
+                                                                    cb({error: 'pause err'}, i);
                                                                 });
-                                                            } else {
-                                                                cb('456');
-                                                            }
-                                                        }
-                                                        if(model.qAddService.cursor.transfer){
-                                                            model.qAddService.cursor.service_counts += model.qAddService.cursor.cost_counts;
+                                                            });
                                                         } else {
-                                                            model.qAddService.cursor.service_counts = model.qAddService.cursor.cost_counts;
+                                                            cb('456', i);
                                                         }
-                                                        model.qListPlannedOperations[i].operation_status = operations.getStatusId('done');
-                                                        model.save();
-                                                        cb({res: 'done'});
-                                                    } else {
-                                                        cb({error: 'no bill_services_accounts'});
                                                     }
+                                                    if(model.qAddService.cursor.transfer){
+                                                        model.qAddService.cursor.service_counts += model.qAddService.cursor.cost_counts;
+                                                    } else {
+                                                        model.qAddService.cursor.service_counts = model.qAddService.cursor.cost_counts;
+                                                    }
+                                                    model.qListPlannedOperations[i].operation_status = operations.getStatusId('done');
+                                                    cb({res: 'done'}, i);
                                                 } else {
-                                                    services.PauseOrResumeService(null, null, model.qListPlannedOperations[i].bill_services_accounts, true, function () {
-                                                        cb({res: 'service paused'});
-                                                    }, function(){
-                                                        cb({error: 'service not paused'});
-                                                    });
+                                                    cb({error: 'no bill_services_accounts'}, i);
                                                 }
                                             } else {
-                                                cb({error: 'Err find qAddService'});
+                                                services.PauseOrResumeService(null, null, model.qListPlannedOperations[i].bill_services_accounts, true, function () {
+                                                    cb({res: 'service paused'}, i);
+                                                }, function(){
+                                                    cb({error: 'service not paused'}, i);
+                                                });
                                             }
+                                        } else {
+                                            cb({error: 'Err find qAddService'}, i);
                                         }
-                                        if (i == model.qListPlannedOperations.length - 1)
-                                            cb({response: "ok"});
-                                    }, function(err) {
-                                        cb(err);
-                                    });
+                                    }
+                                    else {
+                                        cb({err_get_sum: model.qListPlannedOperations[i].account_id}, i);
+                                    }
                                 })(i);
                             }
                         } else {
-                            cb({response: 0});
+                            cb({response: 0}, -1);
                         }
                 };
                 
--- app/Запросы/Услуги/Получение услуги на счету.sql
+++ app/Запросы/Услуги/Получение услуги на счету.sql
@@ -10,4 +10,5 @@ From bill_services_accounts t1
  Where (t1.account_id = :account_id or :account_id is null)
  and (t1.service_id = :service_id or :service_id is null)
  and (t1.bill_services_accounts_id = :service_account_id or :service_account_id is null)
+ and (t3.end_date is null)
 Group by t1.bill_services_accounts_id, t1.account_id, t1.service_id, t1.service_start_date, t1.service_end_date, t1.paused, t1.service_counts, t.bill_services_id, t.service_name, t.locked, t.deleted, t.service_desc, t3.service_type_id, t3.service_days, t3.service_cost, t3.cost_counts
